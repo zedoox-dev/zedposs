47376ef977d1c1f3390450312302e0051b6332c2
ramkesar-app <ramkesarfoods@email.com>
2026-06-21 21:25:41 +0530

    Fix: x error fix recent api

--- app/api/pos/[outletId]/recent/route.ts
+++ app/api/pos/[outletId]/recent/route.ts
@@ -1,4 +1,4 @@
-ximport { NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 
 export async function GET(req: Request, { params }: { params: { outletId: string } }) {
